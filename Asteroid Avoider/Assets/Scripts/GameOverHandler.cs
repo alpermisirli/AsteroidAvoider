f72ffc494efd618975bda3d1daa74f9fffa292fe
RedLegend97 <alpermisirli@gmail.com>
2021-08-30 12:14:31 +0300

    Game Over Handler

---
+++ Asteroid Avoider/Assets/Scripts/GameOverHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverDisplay;
+    [SerializeField] private AsteroidSpawner _asteroidSpawner;
+
+    public void EndGame()
+    {
+        _asteroidSpawner.enabled = false;
+
+        gameOverDisplay.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Asteroid Avoider/Assets/Scripts/PlayerHealth.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerHealth.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private GameOverHandler _gameOverHandler;
+
     public void Crash()
     {
         gameObject.SetActive(false);
+        _gameOverHandler.EndGame();
     }
 }
